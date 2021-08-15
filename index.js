a8e3fa9e77cc88a168cdff18259bde110c5d58ca
rylee816 <65700028+rylee816@users.noreply.github.com>
2021-08-15 09:43:29 -0400

    set timing on card flip and add parcel

--- index.js
+++ index.js
@@ -1,70 +1,96 @@
+import blank from "/images/blank.png";
+import img1 from "/images/IMG_1.jpg"
+import img2 from "/images/IMG_2.jpg"
+import img3 from "/images/IMG_3.jpg"
+import img4 from "/images/IMG_4.jpg"
+import img5 from "/images/IMG_5.jpg"
+import img6 from "/images/IMG_6.jpg"
+import img7 from "/images/IMG_7.jpg"
+import img8 from "/images/IMG_8.jpg"
+
 document.addEventListener("DOMContentLoaded", () => {
 
     //card options
     const cardArray = [
         {
             name: "img1",
-            img : "images/IMG_1.jpg"
+            img : img1,
+            selected: false
         },
         {
             name: "img1",
-            img : "images/IMG_1.jpg"
+            img : img1,
+            selected: false
         },
         {
             name: "img2",
-            img : "images/IMG_2.jpg"
+            img : img2,
+            selected: false
         }, 
         {
             name: "img2",
-            img : "images/IMG_2.jpg"
+            img : img2,
+            selected: false
         },
         {
             name: "img3",
-            img : "images/IMG_3.jpg"
+            img : img3,
+            selected: false
         },
         {
             name: "img3",
-            img : "images/IMG_3.jpg"
+            img : img3,
+            selected: false
         },
         {
             name: "img4",
-            img : "images/IMG_4.jpg"
+            img : img4,
+            selected: false
         },
         {
             name: "img4",
-            img : "images/IMG_4.jpg"
+            img : img4,
+            selected: false
         },
         {
             name: "img5",
-            img : "images/IMG_5.jpg"
+            img : img5,
+            selected: false
         },
         {
             name: "img5",
-            img : "images/IMG_5.jpg"
+            img : img5,
+            selected: false
         },
         {
             name: "img6",
-            img : "images/IMG_6.jpg"
+            img : img6,
+            selected: false
         },
         {
             name: "img6",
-            img : "images/IMG_6.jpg"
+            img : img6,
+            selected: false
         },
         {
             name: "img7",
-            img : "images/IMG_7.jpg"
+            img : img7,
+            selected: false
         },
         {
             name: "img7",
-            img : "images/IMG_7.jpg"
+            img : img7,
+            selected: false
         },
         {
             name: "img8",
-            img : "images/IMG_8.jpg"
+            img : img8,
+            selected: false
         },
         {
             name: "img8",
-            img : "images/IMG_8.jpg"
+            img : img8,
+            selected: false
         },
 
     ];
@@ -81,7 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function createBoard() {
         for(let i = 0; i < cardArray.length; i++){
             let card = document.createElement("img");
-            card.setAttribute("src", "images/blank.png")
+            card.setAttribute("src", blank)
             card.setAttribute("data-id", i);
             card.addEventListener("click", flipCard);
             grid.appendChild(card)
@@ -94,14 +120,15 @@ document.addEventListener("DOMContentLoaded", () => {
         let cards = document.querySelectorAll("img");
         const optionOneId = cardsChosenId[0];
         const optionTwoId = cardsChosenId[1];
+
         if(cardsChosen[0] === cardsChosen[1]){
             alert("You found a match!")
-            // cards[optionOneId].style.display = "none";
-            // cards[optionTwoId].style.display = "none";
             cardsWon.push(cardsChosen);
         } else {
-            cards[optionOneId].setAttribute("src", "images/blank.png")
-            cards[optionTwoId].setAttribute("src", "images/blank.png")
+            setTimeout(() => {
+                cards[optionOneId].setAttribute("src", blank)
+                cards[optionTwoId].setAttribute("src", blank)
+            }, 2000)
             mistakes += 1;
             let triesLeft = 10 - mistakes;
             document.querySelector("#mistakes").innerHTML = " " + triesLeft;
@@ -110,13 +137,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 alert("You lose! You get NOTHING! Good DAY sir!")
                 location.reload()
             } else {
-                alert("Sorry, try again!")
+                setTimeout(() => {
+                    alert("Sorry, try again!") 
+                }, 500)
             }
         }
         cardsChosen = [];
         cardsChosenId = [];
         resultDisplay.textContent = cardsWon.length;
-        if (cardsWon.length === cardArray.length/2){
+        if (cardsWon.length === cardArray.length / 2){
             resultDisplay.textContent = " Congratulations! You found them all!";
             resultDisplay.style.color = "lime";
         }
@@ -126,9 +155,12 @@ document.addEventListener("DOMContentLoaded", () => {
     function flipCard(){
         let cardId = this.getAttribute("data-id");
         cardsChosen.push(cardArray[cardId].name);
+
+        cardArray[cardId].selected = true;
+
         cardsChosenId.push(cardId)
         this.style.transform = "rotateY(180deg)"
-        this.style.transition = "transform .2s ease-in"
+        this.style.transition = "all .4s ease-in"
         this.setAttribute("src", cardArray[cardId].img);
         if(cardsChosen.length === 2){
             setTimeout(checkForMatch, 500)
